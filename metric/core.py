bb44a857a1531e78806c4578c28197fc7403b5ac
a0910257137 <a0910257137@gmail.com>
2022-12-30 14:33:29 +0800

    update metric core dunction

--- metric/core.py
+++ metric/core.py
@@ -1,43 +1,40 @@
 import numpy as np
 from scipy.optimize import linear_sum_assignment
-from typing import List, Tuple
+from typing import Tuple
 
 
-class Metric:
-
-    def __call__(self, gt_idxs: List[int], det_idxs: List[int]) -> Tuple[float, float]:
-        """
-            Feed into the known ground truth and detected peak idxs from one's scan
-            Args:
-                gt_idxs: list of idxs
-                det_idxs: list of idxs
-
-
-        """
-        TP, FP, FN = [], [], []
-        gt_idxs = np.asarray(gt_idxs)
-        det_idxs = np.asarray(det_idxs)
-
-        gt_idxs = gt_idxs[:, None]
-        det_idxs = det_idxs[:, None]
-        cost_matrics = abs(gt_idxs - det_idxs.T)
-        rows, cols = linear_sum_assignment(-cost_matrics, maximize=True)
-        costs = cost_matrics[rows, cols]
-        pairs, picked = [], []
-        zone = 500
-        for r, c, cost in zip(rows, cols, costs):
-            if cost < zone:
-                if c not in picked:
-                    pairs.append([r, c])
-                    picked.append(c)
-
-        TP = len(pairs)
-        FN = np.shape(gt_idxs)[0] - TP
-        FP = det_idxs.shape[0] - TP
-        return self.report(TP, FN, FP)
-
-    def report(self, TP, FN, FP):
+def metric(gt_idxs: np.ndarray, det_idxs: np.ndarray) -> Tuple[float, float]:
+    """
+        Feed into the known ground truth and detected peak idxs from one's scan
+        Args:
+            gt_idxs: list of idxs
+            det_idxs: list of idxs
+        Return:
+            recall, and precision : (float, float)
+    """
+    def report(TP, FN, FP):
         epsilon = 1e-9
         recall = TP / max(TP + FN, epsilon)
         precision = TP / max(TP + FP, epsilon)
         return recall, precision
+
+    TP, FP, FN = [], [], []
+    gt_idxs = np.asarray(gt_idxs)
+    det_idxs = np.asarray(det_idxs)
+    gt_idxs = gt_idxs[:, None]
+    det_idxs = det_idxs[:, None]
+    cost_matrics = abs(gt_idxs - det_idxs.T)
+    rows, cols = linear_sum_assignment(-cost_matrics, maximize=True)
+    costs = cost_matrics[rows, cols]
+    pairs, picked = [], []
+    zone = 500
+    for r, c, cost in zip(rows, cols, costs):
+        if cost < zone:
+            if c not in picked:
+                pairs.append([r, c])
+                picked.append(c)
+
+    TP = len(pairs)
+    FN = np.shape(gt_idxs)[0] - TP
+    FP = det_idxs.shape[0] - TP
+    return report(TP, FN, FP)
